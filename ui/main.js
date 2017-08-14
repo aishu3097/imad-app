ce8fe5e62d4cbe607a23794b424f03231b4f369a
aishu3097 <30777137+aishu3097@users.noreply.github.com>
2017-08-14 10:56:17 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,15 @@
-console.log('Loaded!');
-
-var element=document.getElementById("main-text");
-element.innerHTML = 'New value';
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight() {
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft+'px';
-}
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,50);
+var button = document.getElementById('counter');
+button.onclick=function() {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if(request.readState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://aishu300897.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
